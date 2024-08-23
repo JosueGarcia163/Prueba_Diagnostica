8ef402dbe84fd2060c1da9cd3da0e72b845802fd
amax2019189 <amax-2019189@kinal.edu.gt>
2024-08-23 17:27:44 -0600

    feat:se agrego funcionalidad del ejercicio1.

--- src/org/pruebadiagnostica/views/Menu.java
+++ src/org/pruebadiagnostica/views/Menu.java
@@ -2,6 +2,9 @@ package org.pruebadiagnostica.views;
 
 import java.util.Scanner;
 import org.josuegarcia.controllers.Operaciones;
+import org.randyoscal.model.Desarrollador;
+import org.randyoscal.model.Empleados;
+import org.randyoscal.model.Test;
 import org.ricardofigueroa.system.app;
 
 public class Menu extends Operaciones {
@@ -26,7 +29,16 @@ public class Menu extends Operaciones {
             switch (opcion) {
                 // se llaman los metodos de cada Funcion
                 case 1:
+                    Desarrollador dev = new Desarrollador("Javier", 30, 12000.00, "Java");
+                    Test test = new Test("Manuel", 40, 10000.00, "Funcionalidad");
 
+                    dev.trabajar("Sistema de Gestion");
+                    test.trabajar("Selenium", "Aplicacion Web");
+                    Empleados empleado1 = new Desarrollador("Carlos", 25, 4000.00, "Python");
+                    Empleados empleado2 = new Test("Laura", 29, 3200.00, "Seguridad");
+
+                    empleado1.trabajar(8);
+                    empleado2.trabajar(6);
                     break;
                 case 2:
                     poligono();
