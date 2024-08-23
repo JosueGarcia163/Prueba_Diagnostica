2ff78627a41744bc47d62b16b0b790a292b01a7a
zzzDanielzzz <danieledusc@gmail.com>
2024-08-23 15:45:00 -0600

    build: Se realizaron cambios en la estructura del proyecto

--- src/org/randyoscal/model/Empleados.java
+++ src/org/randyoscal/model/Empleados.java
@@ -6,5 +6,14 @@ public abstract class Empleados {
     private String nombre;
     private int edad;
     private double salario;
+
+    public Empleados() {
+    }
+
+    public Empleados(String nombre, int edad, double salario) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.salario = salario;
+    }
     
 }
